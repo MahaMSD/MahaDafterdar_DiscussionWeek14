24e6e3f04419143d271e8629776e4912bc490fd4
Khaled Dafterdar <Khaled Dafterdar>
2017-04-15 20:05:50 -0400

    history option01

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -124,3 +124,38 @@ $(document).ready(function() {
 
 
 });
+
+/* ------------------------ history timeline ---------------------- */
+// SMOOTH SCROLLING SECTIONS
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+/* --- back to top button --- */
+var $backToTop = $('.back-to-top');
+$backToTop.hide();
+
+
+$(window).on('scroll', function() {
+  if ($(this).scrollTop() > 100) {
+    $backToTop.fadeIn();
+  } else {
+    $backToTop.fadeOut();
+  }
+});
+
+$backToTop.on('click', function(e) {
+  $('html, body').animate({scrollTop: 0}, 500);
+});
+
